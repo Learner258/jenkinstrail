50f53af132cc1ab4f70879a732e54ca557610a33
Learner258 <101538924+Learner258@users.noreply.github.com>
2022-03-15 10:46:56 +0530

    5

---
+++ 2.cs
@@ -0,0 +1,12 @@
+using System;
+
+//namespace har{
+class name
+{
+public static void Main(string[] args)
+{
+	string name=Console.ReadLine();
+	Console.WriteLine("hello "+name+"  let us work");
+	
+}
+}
